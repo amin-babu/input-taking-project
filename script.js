75ba8f21c9ac33c00ab9d5fd7d4fe5d13353e89c
aminbabu12 <badrulaminbabu@gmail.com>
2026-06-06 10:37:58 +0600

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstValue = parseInt(prompt('First Value:'));
+let secondValue = parseInt(prompt('Second Value:'));
+
+document.write(`Summation: ${firstValue + secondValue} <br/>`);
+document.write(`Subtraction: ${firstValue - secondValue} <br/>`);
+document.write(`Multiplication: ${firstValue * secondValue} <br/>`);
+document.write(`Division: ${firstValue / secondValue} <br/>`);
+document.write(`Exponential: ${firstValue ** secondValue} <br/>`);
